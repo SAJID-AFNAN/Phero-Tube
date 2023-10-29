19e9508eb5a60153bd33a1b176cb04c0d01dae03
SAJID-AFNAN <sajidafnan91@gmail.com>
2023-10-29 23:26:57 +0600

    Added loadingSpiner

--- JS/index.js
+++ JS/index.js
@@ -7,22 +7,24 @@ const showButton = async () => {
     const cardButton = document.getElementById('card-button')
 
     AllData.forEach(singleData => {
-        // console.log(singleData)
+        console.log(singleData)
         const div = document.createElement('div')
         div.innerHTML = `
-        <button class="btn rounded-md">${singleData.category}</button>
+        <button onclick="showCard('${singleData.category_id}')" class="btn rounded-md">${singleData.category}</button>
         `
         cardButton.appendChild(div);
     })
 }
 
-const showCard = async () => {
-    const response = await fetch('https://openapi.programming-hero.com/api/videos/category/1000')
+const showCard = async (id) => {
+    toggleLoading(true)
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
     const data = await response.json()
-
+    console.log(id)
     let cardData = data.data
     console.log(cardData)
     const cardContainer = document.getElementById('card-container')
+    cardContainer.innerHTML = '';
 
     cardData.forEach(singleCard => {
         const div = document.createElement('div');
@@ -47,10 +49,19 @@ const showCard = async () => {
         `
         cardContainer.appendChild(div)
     })
+    toggleLoading(false)
+}
 
+const toggleLoading = (isloading) => {
+    const loadingSpiner = document.getElementById('loading-spiner')
 
-
+    if (isloading) {
+        loadingSpiner.classList.remove('hidden')
+    }
+    else {
+        loadingSpiner.classList.add('hidden')
+    }
 }
 
 showButton();
-showCard()
+// showCard()
