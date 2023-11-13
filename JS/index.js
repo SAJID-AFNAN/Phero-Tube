bc95383c4d7e32fb121fe1a64e3d5cc95941a492
SAJID-AFNAN <sajidafnan91@gmail.com>
2023-11-14 00:43:55 +0600

    Added sorting section

--- JS/index.js
+++ JS/index.js
@@ -10,20 +10,44 @@ const showButton = async () => {
         // console.log(singleData)
         const div = document.createElement('div')
         div.innerHTML = `
-        <button onclick="showCard('${singleData.category_id}')" class="btn rounded-md">${singleData.category}</button>
+        <button onclick="loadData('${singleData.category_id}')" class="btn rounded-md">${singleData.category}</button>
         `
         cardButton.appendChild(div);
     })
 }
 
-const showCard = async (id) => {
-    toggleLoading(true)
-    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`)
-    const data = await response.json()
-    // console.log(id)
-    let cardData = data.data
-    console.log(cardData)
-    const cardContainer = document.getElementById('card-container')
+
+//Card fetch section
+let publicData = null;
+
+const loadData = async (id) => {
+    toggleLoading(true);
+    const response = await fetch(`https://openapi.programming-hero.com/api/videos/category/${id}`);
+    const data = await response.json();
+    publicData = data.data;
+    console.log(publicData);
+    showCard(data.data)
+}
+
+
+//Sorting section
+const sortByViews = () => {
+    const cardData = publicData;
+
+    cardData.sort((a, b) => {
+        const viewsA = parseInt(a.others?.views.replace('K', '')) || 0;
+        const viewsB = parseInt(b.others?.views.replace('K', '')) || 0;
+
+        return viewsB - viewsA;
+    })
+    showCard(cardData);
+}
+
+
+//any Card show section
+const showCard = async (cardData) => {
+    // console.log(cardData)
+    const cardContainer = document.getElementById('card-container');
     cardContainer.innerHTML = '';
 
     cardData.forEach(singleCard => {
@@ -52,6 +76,8 @@ const showCard = async (id) => {
     toggleLoading(false)
 }
 
+
+//Loading section
 const toggleLoading = (isloading) => {
     const loadingSpiner = document.getElementById('loading-spiner')
 
@@ -64,11 +90,14 @@ const toggleLoading = (isloading) => {
 }
 
 
+
 //Go to the Blog.html page
 function blogbutton() {
     window.location.href = 'Blog.html';
 }
 
 
+
 showButton();
+// sortByViews();
 // showCard()
